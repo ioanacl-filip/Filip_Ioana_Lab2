1735e42bc60d2058ca20408443567e466760102f
ioanacl-filip <filipioanaclaudia@yahoo.ro>
2024-11-07 09:10:00 +0200

    laborator 4 cerinte

---
+++ Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,10 @@
+﻿using Filip_Ioana_Lab2.Models;
+namespace Filip_Ioana_Lab2.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Book> Books { get; set; }
+        public IEnumerable<Category> Category { get; set; }
+
+    }
+}
---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,9 @@
+﻿using Filip_Ioana_Lab2.Models;
+namespace Filip_Ioana_Lab2.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
